6d99d4508b5b0b3ebe7bf6a67cc55f8a9cc42975
neilshirsat <neilshirsat2015@gmail.com>
2022-04-11 23:09:50 -0500

    init

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,45 +1,33 @@
 import Hexagon from "./Hexagon";
-import './board.less'
+import './board.scss'
 import React, { Dispatch, useEffect } from "react";
 import { BoardData } from "./App";
 
 export const scaleFactor = 4;
 
 const points: {
-    x: number;
-    y: number;
+    nodeId: number
 }[] = [
-    { x: 27 * scaleFactor, y: 10 * scaleFactor }, //1
-    { x: 45 * scaleFactor, y: 10 * scaleFactor }, //2
-    { x: 63 * scaleFactor, y: 10 * scaleFactor }, //3
-    { x: 18 * scaleFactor, y: 25 * scaleFactor }, //4
-    { x: 36 * scaleFactor, y: 25 * scaleFactor }, //5
-        //Center
-        //{ x: 45 * scaleFactor, y: 40 * scaleFactor },
-        //Top
-        //{ x: 45 * scaleFactor, y: 10 * scaleFactor },
-        //Bottom
-        //{ x: 45 * scaleFactor, y: 70 * scaleFactor },
-        //Inside Circle
-        //{ x: 54 * scaleFactor, y: 55 * scaleFactor }, //Bottom Right
-        //{ x: 36 * scaleFactor, y: 55 * scaleFactor }, //Bottom Left
-        //{ x: 27 * scaleFactor, y: 40 * scaleFactor }, //Left
-        //{ x: 36 * scaleFactor, y: 25 * scaleFactor }, //Top Left
-        //{ x: 54 * scaleFactor, y: 25 * scaleFactor }, //Top Right
-        //{ x: 63 * scaleFactor, y: 40 * scaleFactor }, //Right
-        //Outside Circle
-        //{ x: 81 * scaleFactor, y: 40 * scaleFactor }, //Right Right
-        //{ x: 9 * scaleFactor, y: 40 * scaleFactor }, //Left Left
-        //Compose Rose 1-8 Starting from North->E->S->W
-        //{ x: 63 * scaleFactor, y: 10 * scaleFactor },
-        //{ x: 72 * scaleFactor, y: 25 * scaleFactor },
-        //{ x: 72 * scaleFactor, y: 55 * scaleFactor },
-        //{ x: 63 * scaleFactor, y: 70 * scaleFactor },
-        //{ x: 27 * scaleFactor, y: 70 * scaleFactor },
-        //{ x: 18 * scaleFactor, y: 55 * scaleFactor },
-        //{ x: 18 * scaleFactor, y: 25 * scaleFactor },
-        //{ x: 27 * scaleFactor, y: 10 * scaleFactor },
-    ]
+    { nodeId: 1 },
+    { nodeId: 2 },
+    { nodeId: 3 },
+    { nodeId: 4 },
+    { nodeId: 5 },
+    { nodeId: 6 },
+    { nodeId: 7 },
+    { nodeId: 8 },
+    { nodeId: 9 },
+    { nodeId: 10 },
+    { nodeId: 11 },
+    { nodeId: 12 },
+    { nodeId: 13 },
+    { nodeId: 14 },
+    { nodeId: 15 },
+    { nodeId: 16 },
+    { nodeId: 17 },
+    { nodeId: 18 },
+    { nodeId: 19 },
+]
 
 const Board: React.FC<{
     boardData: BoardData,
@@ -50,28 +38,19 @@ const Board: React.FC<{
 
     })
 
-    return (<div className="board-root">
-        <svg
-            className="board" 
-            viewBox={`0 0 ${90 * scaleFactor} ${80 * scaleFactor}`}
-            style={{
-            }}>
-            <defs>
-                <pattern id="background" style={{padding: '1rem'}} height="100%" width="100%" patternContentUnits="objectBoundingBox">
-                    <image height="1" width="1" preserveAspectRatio="none" href="https://www.thespruce.com/thmb/OBzH-HbeogCLRTyu2KGFAP6jLgc=/1500x1500/filters:fill(auto,1)/_hero_SQ_Catan-Board-Game-1-4e3bc64d68ea4b2d99840e423879cb9f.jpg" />
-                </pattern>
-            </defs>
-            {points.map(e =>
-                <>
-                    <Hexagon x={e.x} y={e.y}>
-                    
-                    </Hexagon>
-                </>
-            )}
-            <line x1={36 * scaleFactor} y1={45 * scaleFactor} x2={36 * scaleFactor} y2={35 * scaleFactor} stroke="blue" width="2px"></line>
-            
-        </svg>
-    </div>)
+    return (<main className="board-root">
+        <ul className="container">
+            {
+                points.map(hex => {
+                    return (
+                        <Hexagon x={0} y={0}>
+
+                        </Hexagon>
+                    )
+                })
+            }
+        </ul>
+    </main>)
 }
 
 export default Board;
--- src/Hexagon.tsx
+++ src/Hexagon.tsx
@@ -5,15 +5,13 @@ const Hexagon: React.FC<{
     x: number,
     y: number,
 }> = (props) => {
-    return (<g>
-        <polygon
-            className="hex"
-            fill="url(#background)"
-            points={`${9 * scaleFactor},${-5 * scaleFactor} ${0 * scaleFactor},${-10 * scaleFactor} ${-9 * scaleFactor},${-5 * scaleFactor} ${-9 * scaleFactor},${5 * scaleFactor} ${0 * scaleFactor},${10 * scaleFactor} ${9 * scaleFactor},${5 * scaleFactor}`}
-            transform={`translate(${props.x}, ${props.y})`}>
-            {props.children}
-        </polygon>
-    </g>)
+    return (<li
+        className="hex-wrapper">
+        <div
+            className="hex">
+            Hello
+        </div>
+    </li>)
 }
 
 export default Hexagon;
